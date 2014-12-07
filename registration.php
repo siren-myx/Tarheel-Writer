ef0c4880ea3f299e6f4358317200e066c8abeba9
KJ Moon <kjmoon@unc.edu>
2014-12-07 00:16:03 -0500

    changed sign up.

--- registration.php
+++ registration.php
@@ -51,7 +51,7 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
          <form role="form">
-            <h2>Please Sign Up <small>It's free and always will be.</small></h2>
+            <h2>Sign up & Write!</h2>
             <hr class="colorgraph">
             <div class="form-group">
                <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="4">

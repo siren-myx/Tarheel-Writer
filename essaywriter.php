b986f2a20d46aa6f10e51d70bcf7989c400a4276
enigward <edward.richman@sbcglobal.net>
2014-12-09 12:52:49 -0500

    Update essaywriter.php

--- essaywriter.php
+++ essaywriter.php
@@ -68,7 +68,7 @@ if(!isset($_SESSION["sess_user"])){
                 	<label>Number of body paragraphs:</label>
                     <input id="out" type="number" class="form-control" value="3" min="1" max="10" step="1">
                     </div>
-                    <button class="btn btn-default btn-lg" onclick="outline()">Next</button>
+                    <button style="color:black" onclick="outline()">Next</button>
                 </div><br>
 
             <?php
